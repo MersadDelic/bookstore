7ba668eef27d91292f2e429c17636f1d99941492
Mersad Dela <developer78991@gmail.com>
2020-10-18 12:48:42 +0200

    citanje iz json fajla - hardkodirane knjige

--- src/app/Book/book.service.ts
+++ src/app/Book/book.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {Book} from './book';
 import {catchError, tap} from 'rxjs/operators';
@@ -17,29 +17,8 @@ export class BookService {
 
   getAllBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.booksUrl)
-      /*.pipe(
-        map((response) => response),
-        catchError((err) => throwError(err))
-      );*/
       .pipe(
         tap(data => console.log('All: ' + JSON.stringify(data))),
-        catchError(this.handleError));
-  }
-
-
-  private handleError(err: HttpErrorResponse): Observable<never> {
-    // in a real world app, we may send the server to some remote logging infrastructure
-    // instead of just logging it to the console
-    let errorMessage = '';
-    if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      errorMessage = `An error occurred: ${err.error.message}`;
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-  }
-    console.error(errorMessage);
-    return throwError(errorMessage);
+        catchError((err) => throwError(err)));
   }
 }
